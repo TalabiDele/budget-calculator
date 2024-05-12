8debe6150070eb0a998b5886195bf1296ce0392d
talabidele <talabibamidele7@gmail.com>
2024-05-12 22:33:50 +0100

    Added go back function

--- src/components/StepNav.tsx
+++ src/components/StepNav.tsx
@@ -1,9 +1,20 @@
 import { Box, Icon, Text } from '@chakra-ui/react'
 import { FaArrowLeftLong } from 'react-icons/fa6'
 import useMyContext from '../hooks/useMyContext'
+import { useNavigate } from 'react-router'
 
 const StepNav = () => {
-	const { step } = useMyContext()
+	const { step, setStep } = useMyContext()
+
+	const navigate = useNavigate()
+
+	const handleNav = () => {
+		if (step === 1) {
+			navigate(-1)
+		} else if (step > 1) {
+			setStep(step - 1)
+		}
+	}
 
 	return (
 		<Box
@@ -12,7 +23,12 @@ const StepNav = () => {
 			alignItems={'center'}
 			justifyContent={'space-between'}
 		>
-			<Icon as={FaArrowLeftLong} fontSize={'1rem'} cursor={'pointer'} />
+			<Icon
+				as={FaArrowLeftLong}
+				fontSize={'1rem'}
+				cursor={'pointer'}
+				onClick={handleNav}
+			/>
 
 			<Text color={'gray'} fontSize={'sm'} fontWeight={'medium'}>
 				<span style={{ color: '#000' }}>{step}</span>/3
